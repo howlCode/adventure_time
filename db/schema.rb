f7568b65b249f2371252d2252410762fcd69a5ee
howlCode <miromanyth@gmail.com>
2018-08-05 09:15:46 -0400

    Add user_id column to Story model. All tests green

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_203321) do
+ActiveRecord::Schema.define(version: 2018_08_05_131353) do
 
   create_table "stories", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Story, type: :model do
 
+  let(:user) {User.create!(email: 'test@example.com', password: 'testing123')}
+
   it "is valid with valid attributes" do
-    story = Story.create!(title: "test title", body: "test body")
+    story = Story.create!(title: "test title", body: "test body", user: user)
   end
 
   it "is not valid without a title and/or body" do
-    story = Story.new(title: nil, body: nil)
+    story = Story.new(title: nil, body: nil, user: nil)
     expect(story).to_not be_valid
   end
 

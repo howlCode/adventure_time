db330980a69bc34733593e26e9d9ea00a545253e
howlCode <miromanyth@gmail.com>
2018-08-07 10:48:43 -0400

    Add Arc model test. Remove color-styles for now. All tests green

--- spec/models/arc_spec.rb
+++ spec/models/arc_spec.rb
@@ -9,5 +9,10 @@ RSpec.describe Arc, type: :model do
     arc = Arc.create!(body: 'testing', story: story, user: user)
     expect(arc).to be_valid
   end
+
+  it "is not valid without a body" do
+    bad_arc = Arc.new(body: nil, story: story, user: user)
+    expect(bad_arc).to_not be_valid
+  end
   
 end

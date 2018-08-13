349814f10c0de4eb78a004c925766611418e3fb2
howlCode <miromanyth@gmail.com>
2018-08-13 10:19:15 -0400

    Remove acts_as_voteable gem. Too much. TODO add Vote model and reference arcs and users

--- app/models/arc.rb
+++ app/models/arc.rb
@@ -1,6 +1,5 @@
 class Arc < ApplicationRecord
   include Snippers
-  acts_as_votable
   belongs_to :story
   belongs_to :user
   validates :body, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  acts_as_voter
   has_many :stories
   has_many :arcs
   # Include default devise modules. Others available are:

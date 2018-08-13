fafa1e4f3f4228518dea2b7519cd7f46122d4a2c
howlCode <miromanyth@gmail.com>
2018-08-13 09:58:02 -0400

    Add act_as_voteable gem. Add acts_as_voteable to Arc model. Add acts_as_voter to User model.

--- app/models/arc.rb
+++ app/models/arc.rb
@@ -1,5 +1,6 @@
 class Arc < ApplicationRecord
   include Snippers
+  acts_as_votable
   belongs_to :story
   belongs_to :user
   validates :body, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  acts_as_voter
   has_many :stories
   has_many :arcs
   # Include default devise modules. Others available are:

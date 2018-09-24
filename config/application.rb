e3e0dff05dde38f5ac5ed38eb4d3ee0ec1da9a17
howlCode <miromanyth@gmail.com>
2018-09-24 11:38:24 -0400

    cors.......

--- config/application.rb
+++ config/application.rb
@@ -17,11 +17,10 @@ module AdventureTime
     # the framework and any gems in your application.
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'localhost:8080'
     
         resource '*',
           headers: :any,
-          credentials: false,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
